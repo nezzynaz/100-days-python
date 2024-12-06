c032b18aca717c15ed7f3a8e29e3271743b8e77e
Nezzy <23644622+nezzynaz@users.noreply.github.com>
2024-12-06 14:01:09 -0600

    day24 pt2: finished project

--- day24/main.py
+++ day24/main.py
@@ -1,10 +1,19 @@
 '''Letter Writing Program'''
 
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
+PLACEHOLDER = "[name]"
 
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+with open("Input\\Names\\invited_names.txt", "r") as names_file:
+    names = names_file.readlines()
+
+with open("Input\\Letters\\starting_letter.txt", "r") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"Output\\ReadyToSend\\letter_for_{stripped_name}.txt", "w") as completed_letter:
+            completed_letter.write(new_letter)
+
+# Big takeaways:
+# You can use an f string to name the file you want to create, as seen on line 13
+# The strip function will remove the new line character, as seen on line 11
+# My logic was correct throughout my initial attempt, and everything I didn't know is recorded here.
